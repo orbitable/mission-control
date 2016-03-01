ed635363bee5fc3fa8e306e9330bbe6bb4d43a4a
Dillon Wood <dillon.wood@outlook.com>
2016-02-29 23:16:33 -0700

    added basic logger

---
+++ logger.js
@@ -0,0 +1,13 @@
+﻿var winston = require('winston');
+
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)()
+    ]
+});
+
+logger.handleExceptions();
+
+logger.info(Date() + ': log service start');
+
