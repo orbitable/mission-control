a7ce5026a1f52f25772ca49668ef2c288904589e
Dillon Wood <dillon.wood@outlook.com>
2016-03-06 18:11:52 -0700

    added skeletal session endpoint

---
+++ controllers/session.js
@@ -0,0 +1,66 @@
+/* Copyright 2016 Orbitable Team Members
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+var util = require('util');
+
+exports.install = function() {
+  logger.debug("Installing session restful route");
+
+  
+  F.restful('/session', [], session_query, session_post, session_delete);
+};
+
+function session_delete(id) {
+  logger.debug("Removing session " + id);
+    var self = this;
+  //Add delete functionality
+}
+
+
+function session_query() {
+  logger.debug("Getting sessions");
+
+  var self = this;
+  //Add query later on. I think a session model must be implemented first?
+}
+
+
+function session_post(id) {
+  var self = this;
+  
+
+  if (id) {
+    var updates = self.json;
+    logger.debug("Updating a session with id %s", id);
+
+    //Add post information. STILL CONFUSED HOW TO SEND DATA WITHOUT IT! 
+
+      self.json(doc);
+    });
+  } else {
+
+    logger.debug("Saving session id " + id);
+
+    Simulation.create(self.body, function(err, doc) {
+
+      if (err) {
+        logger.error("Unable to create session: " + err);
+        self.throw400(err);
+        return;
+      }
+
+      self.json(doc);
+    });
+  }
+}
